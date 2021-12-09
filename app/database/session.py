88f7e813271ed3daa36362c3422df32073b6cc35
Sebastian <seumamu@gmail.com>
2021-12-09 06:28:41 -0500

    ordenacin de directorios

--- app/database/session.py
+++ app/database/session.py
@@ -5,3 +5,4 @@ from app.core.config  import settings
  
 engine = create_engine(settings.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base declarative_base ()
